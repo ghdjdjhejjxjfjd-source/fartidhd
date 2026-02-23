de92eb4408a6ef7f7c8e8bafda0bc31cf1e49d29
Fayrat11 <etoakrasavaa@gmail.com>
2026-02-23 10:27:32 +0600

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,1 +1,12 @@
- 
+from .config import api
+
+# Импортируем все маршруты
+from . import routes
+from . import stars
+from . import image
+
+# Функция для отправки логов (должна быть доступна везде)
+from .config import send_log_to_group
+
+# Экспортируем api для использования в main.py
+__all__ = ['api']
