1904ea0114f0af92e1db11d5f69a294ee70cac8d
Fayrat11 <etoakrasavaa@gmail.com>
2026-02-23 10:52:02 +0600

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,1 +1,11 @@
- 
+from .handlers import start, on_button, handle_message
+from .config import send_log_http, build_start_log
+
+# Экспортируем всё нужное для bot.py
+__all__ = [
+    'start',
+    'on_button', 
+    'handle_message',
+    'send_log_http',
+    'build_start_log'
+]
