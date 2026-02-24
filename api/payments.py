0ccecfae92aca47f7a33120d057978dcb73c3e49
Fayrat11 <etoakrasavaa@gmail.com>
2026-02-24 22:06:44 +0600

    Update payments.py

--- api/payments.py
+++ api/payments.py
@@ -1,12 +1,2 @@
 # api/payments.py
-# Этот файл перенаправляет всё в payments_db и star_packages
-
-from .payments_db import (
-    get_balance, add_stars, spend_stars, get_top_users, reset_balance
-)
-from .star_packages import get_packages, get_package, STAR_PACKAGES
-
-__all__ = [
-    'get_balance', 'add_stars', 'spend_stars', 'get_packages', 'get_package',
-    'get_top_users', 'reset_balance', 'STAR_PACKAGES'
-]
+from .payments_db import *
