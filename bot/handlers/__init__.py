fcbf4de8f5a37827233c0c8d43784eab0648a533
Fayrat11 <etoakrasavaa@gmail.com>
2026-03-12 10:34:27 +0600

    Create __init__.py

---
+++ bot/handlers/__init__.py
@@ -0,0 +1,27 @@
+from .start import start
+from .router import on_button, open_tab, navigation_stack
+from .navigation import back_to_previous, back_to_menu, ignore
+
+from .tabs.profile import show_profile
+from .tabs.help import show_help
+from .tabs.status import show_status
+from .tabs.ref import show_ref
+from .tabs.support import show_support
+from .tabs.buy_stars import show_buy_stars, buy_stars_package
+
+__all__ = [
+    'start',
+    'on_button',
+    'open_tab',
+    'navigation_stack',
+    'back_to_previous',
+    'back_to_menu',
+    'ignore',
+    'show_profile',
+    'show_help',
+    'show_status',
+    'show_ref',
+    'show_support',
+    'show_buy_stars',
+    'buy_stars_package',
+]
