2684790ce980ecf3fbaf6476634ad440ce490908
Fayrat11 <etoakrasavaa@gmail.com>
2026-03-12 10:29:39 +0600

    Update start.py

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,1 +1,21 @@
-т
+from telegram import Update
+from telegram.ext import ContextTypes
+
+from bot.utils import send_fresh_menu
+from bot.config import build_start_log, send_log_http
+
+navigation_stack = {}  # временно тут, потом перенесем
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    send_log_http(build_start_log(update))
+    
+    user = update.effective_user
+    uid = user.id if user else 0
+    if not uid:
+        return
+    
+    # Очищаем навигацию при старте
+    if uid in navigation_stack:
+        del navigation_stack[uid]
+    
+    await send_fresh_menu(context.bot, uid)
