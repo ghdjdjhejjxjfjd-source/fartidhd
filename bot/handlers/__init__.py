d38dc525f9bf7e65664af2af7135ab6281f4ed69
Fayrat11 <etoakrasavaa@gmail.com>
2026-03-14 14:35:13 +0600

    Add files via upload

---
+++ bot/handlers/__init__.py
@@ -0,0 +1,37 @@
+from .start import start
+from .router import on_button, open_tab
+from .state import navigation_stack
+from .navigation import back_to_previous, back_to_menu, ignore
+from .tabs.profile import show_profile
+from .tabs.help import show_help
+from .tabs.status import show_status
+from .tabs.ref import show_ref
+from .tabs.support import show_support
+from .tabs.buy_stars import show_buy_stars, buy_stars_package
+from .tabs.style import show_style_settings, set_style
+from .tabs.ai_lang import show_ai_lang_settings, set_ai_lang
+
+# Импортируем handle_message из переименованного файла
+from bot.old_handlers import handle_message
+
+__all__ = [
+    'start',
+    'on_button',
+    'open_tab',
+    'navigation_stack',
+    'back_to_previous',
+    'back_to_menu',
+    'ignore',
+    'show_profile',
+    'show_help',
+    'show_status',
+    'show_ref',
+    'show_support',
+    'show_buy_stars',
+    'buy_stars_package',
+    'show_style_settings',
+    'set_style',
+    'show_ai_lang_settings',
+    'set_ai_lang',
+    'handle_message',
+]
