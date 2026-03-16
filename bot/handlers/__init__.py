c6868d5fff95dfc72a2c17a668abd6761c7b04a5
Fayrat11 <etoakrasavaa@gmail.com>
2026-03-16 15:59:48 +0600

    Update __init__.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -12,6 +12,7 @@ from .tabs.style import show_style_settings, set_style
 from .tabs.ai_lang import show_ai_lang_settings, set_ai_lang
 from bot.old_handlers import handle_message
 from bot.menu import main_menu_for_user
+from bot.support import support_start, forward_to_support, handle_support_reply
 
 __all__ = [
     'start',
@@ -34,4 +35,7 @@ __all__ = [
     'set_ai_lang',
     'handle_message',
     'main_menu_for_user',
+    'support_start',
+    'forward_to_support',
+    'handle_support_reply',
 ]
