805762c0b67d40e31f7ea5f4031d7ba8a20116fd
ghdjdjhejjxjfjd-source <ghdjdjhejjxjfjd@gmail.com>
2026-03-22 10:28:28 +0600

    Update __init__.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,7 +1,8 @@
+# bot/handlers/__init__.py - УБИРАЕМ old_handlers
 from .start import start
 from .router import on_button, open_tab
-from .state import navigation_stack
 from .navigation import back_to_previous, back_to_menu, ignore
+from .state import navigation_stack
 from .tabs.profile import show_profile
 from .tabs.help import show_help
 from .tabs.status import show_status
@@ -10,9 +11,7 @@ from .tabs.support import show_support
 from .tabs.buy_stars import show_buy_stars, buy_stars_package
 from .tabs.style import show_style_settings, set_style
 from .tabs.ai_lang import show_ai_lang_settings, set_ai_lang
-from bot.old_handlers import handle_message
-from bot.menu import main_menu_for_user
-from bot.support import support_start, forward_to_support  # ← УБРАЛ handle_support_reply
+from bot.utils import handle_message  # ← импортируем handle_message из utils
 
 __all__ = [
     'start',
@@ -34,7 +33,4 @@ __all__ = [
     'show_ai_lang_settings',
     'set_ai_lang',
     'handle_message',
-    'main_menu_for_user',
-    'support_start',
-    'forward_to_support',
 ]
